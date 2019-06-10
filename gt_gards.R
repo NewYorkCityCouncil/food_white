c57958ca982fe6b35d0d85289571e7d5cd23ae96
Brook Frye <bfrye@council.nyc.gob>
2019-06-10 10:47:15 -0400

    add gt gards

---
+++ gt_gards.R
@@ -0,0 +1,19 @@
+# let's look at greenthumb gardens 
+# maybe also vacant lots for potential gardens?
+
+library(data.table)
+library(leaflet)
+library(ggplot2)
+library(councildown)
+
+# load data 
+gt_gards <- fread("https://data.cityofnewyork.us/resource/yes4-7zbb.csv")
+
+# let's map 
+
+gt <- leaflet(gt_gards) %>% 
+  addProviderTiles("CartoDB.Positron") %>%  
+  addCircles(lng = ~longitude, 
+             lat = ~latitude, 
+             popup = ~garden_name)
+  
